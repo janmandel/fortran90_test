5351a411c861b4459faa3ef98b374fd267710ee0
Jan Mandel <jan.mandel@gmail.com>
2018-10-30 14:05:12 -0600

    test modules

--- test.f90
+++ test.f90
@@ -1,7 +1,17 @@
   program test
   use test_mod
   implicit none
+! *** local ***
+  integer:: level, levels
+  parameter(levels=5)
   real:: x,xx
+
+! *** executable ***  
+  type(mg), dimension(levels) :: p
+  do level=1,levels
+     allocate(p(level)%u_h(10,10))
+  enddo
+   
   call sub(x)
   print *,'hi there', x
   end
--- test_mod.f90
+++ test_mod.f90
@@ -1,8 +1,21 @@
   module test_mod
+  ! variables
+  type mg
+     double precision, pointer, dimension(:,:):: C_h_stencil
+     double precision, pointer, dimension(:,:):: I_2h_h_stencil
+     double precision, pointer, dimension(:,:):: I_h_2h_stencil
+     double precision, pointer, dimension(:,:):: u_h, f_h, r_h
+  end type mg
+
   contains
+  ! subroutines and functions
   subroutine sub(y)
+  ! purpose: test assignment
+  ! input: none
+  ! output: y
+  implicit none
+  real, intent(out):: y
   y=1
-  zz=9989
   end subroutine sub
   end module test_mod
 
